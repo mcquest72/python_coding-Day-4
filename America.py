206a846f003f9edfa0650b156d84fd07585006e6
McQuest-72 <mcquest2347@gmail.com>
2024-03-17 13:43:34 -0800

    List of states in America created

---
+++ America.py
@@ -0,0 +1,13 @@
+
+# This program details the names of states in USA.
+
+states_of_America = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_America[13])
+print()
+
+# print the last item in the list
+# negative integers are used for this purpose
+
+print(states_of_America[-3])
+print()
