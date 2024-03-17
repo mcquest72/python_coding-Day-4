d07367867243c94945f65410538ef83b3232d9dc
McQuest-72 <mcquest2347@gmail.com>
2024-03-17 11:17:21 -0800

    virtual coin program created... Added code to TestCode.py

--- TestCode.py
+++ TestCode.py
@@ -2,5 +2,15 @@
 
 import random
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+random_interger = random.randint(1, 10)
+print()
+print(random_interger)
+print()
+
+random_float = random.random() * 5
+print(random_float)
+print()
+
+love_score = random.randint(1, 100)
+print(f'Your love score is {love_score}')
+print()
---
+++ day-4-1-exercise-Virtual_coin.py
@@ -0,0 +1,14 @@
+
+# This is a virtual coin toss program
+
+import random
+
+toss = random.randint(0, 1)
+print()
+
+if toss == 1:
+    print('Heads')
+else:
+    print('Tails')
+
+print()
