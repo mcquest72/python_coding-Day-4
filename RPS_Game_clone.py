10d8508849226b482edfad05998fcb5be379694b
McQuest-72 <mcquest2347@gmail.com>
2024-03-19 08:44:02 -0800

    logic code added to program and executed successfully.

--- RPS_Game_clone.py
+++ RPS_Game_clone.py
@@ -31,29 +31,40 @@ scissors = '''
 ---.__(___)
 '''
 
+print()
 game_images = [rock, paper, scissors]
 
-user_choice = int(input('what do you choose? Type 0 for rock, 1 for paper or 2 for scissors. \n'))
+user_input = input('what do you choose? Type 0 for rock, 1 for paper or 2 for scissors. \n')
+
+user_choice = int(user_input)
 print(game_images[user_choice])
 
 computer_choice = random.randint(0, 2)
 print('Computer chose:')
 print(game_images[computer_choice])
 
-if user_choice >= 2 or user_choice < 0:
-    print('You typed an invalid number, you lose!')
-
-elif user_choice == 0 and computer_choice == 2:
+if user_choice == 0 and computer_choice == 2:
     print('You win!')
 
 elif computer_choice == 0 and user_choice == 2:
     print('You lose')
 
-elif computer_choice > user_choice:
+elif user_choice == 2 and computer_choice == 1:
+    print('You win!')
+
+elif computer_choice == 2 and user_choice == 1:
     print('You lose')
 
-elif user_choice > computer_choice:
+elif user_choice == 1 and computer_choice == 0:
     print('You win!')
+
+elif computer_choice == 0 and user_choice == 1:
+    print('You lose')
 
 elif computer_choice == user_choice:
     print('It\'s a draw')
+
+else: 
+    print('You typed an invalid number, you lose!')
+
+print()
